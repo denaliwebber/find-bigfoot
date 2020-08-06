a70e8a7da20a13092f55743d526b0f321c8c603d
denali.webber <denali.webber@gmail.com>
2020-08-06 10:35:55 -0700

    Added comments to script file.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// is called after page has loaded to ensure the bigfoot image doesn't appear first
 function loadBigFoot() {
   let bigFoot = document.getElementById("bigFoot");
   bigFoot.setAttribute("src", "bigfoot.png");
 }
 
+// alerts user they have found bigfoot and moves image to new random location
 function moveBigFoot() {
   alert('Woohoo, you win! You found bigfoot!');
   let bigFoot = document.getElementById("bigFoot");
